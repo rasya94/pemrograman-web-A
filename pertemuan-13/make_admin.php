6043afa2d4ab3da931373d5d4184068325425277
Rasya <school.rasya@gmail.com>
2025-12-26 11:11:24 +0700

    Delete pertemuan-13/make_admin.php

--- pertemuan-13/make_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost","root","","news_portal");
-$conn->query("
-INSERT INTO users (username,password,fullname)
-VALUES (
- 'admin',
- '".password_hash("admin123", PASSWORD_BCRYPT)."',
- 'Administrator'
-)");
